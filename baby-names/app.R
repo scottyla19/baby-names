4d7d6d30ec9c6f49e025a487b4dd627741de4fbf
Scott <scott@iMac.local>
2018-05-09 06:09:17 -0600

    added popular names tab

--- baby-names/app.R
+++ baby-names/app.R
@@ -1,8 +1,13 @@
 library(shiny)
 library(tidyverse)
+library(data.table)
 
-df <- read.csv("names-all-years.csv")
-df <- select( df, -(X))
+# df <- read.csv("names-all-years.csv")
+
+# use data.table fread for increased speed
+df <- fread("names-all-years.csv") %>% as.data.frame()
+df <- select( df, -(V1))
+head(df)
 sortedUniqueNames <- unique(df$Name) %>% sort()
 
 # Define UI for application that draws a histogram
@@ -10,31 +15,64 @@ ui <- fluidPage(
    
    # Application title
    titlePanel("Baby Name Analyzer"),
+   tabsetPanel(type = "tabs",
+               tabPanel("Popular Names", fluid = TRUE,
+                 sidebarLayout(
+                   sidebarPanel(
+                    
+                     sliderInput("year_range",
+                                 "Range of Years",
+                                 min = 1880,
+                                 max = 2016,
+                                 step = 1,
+                                 value = c(2000, 2016))
+                     ),
+                  
+                   
+                   # Show a plot of the generated distribution
+                   mainPanel(
+                     plotOutput("popular_plot")
+                     
+                   )
+                 )
+               ),
+               tabPanel("Names Over Time", fluid = TRUE,
+                 sidebarLayout(
+                   sidebarPanel(
+                     radioButtons("selected_gender", label = "Select a gender:",
+                                  choices = list("Female" = "F", "Male" = "M", "Either" = "E"), 
+                                  selected = "F"),
+                     selectInput("selected_name",
+                                 "Select a Name:",
+                                 sortedUniqueNames
+                     )
+                   ),
+                   
+                   mainPanel(
+                     plotOutput("nameTimeSeries"),
+                     textOutput("test")
+                   )
+                 )
+               )
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        radioButtons("selected_gender", label = "Select a gender:",
-                     choices = list("Female" = "F", "Male" = "M", "Either" = "E"), 
-                     selected = "F"),
-         selectInput("selected_name",
-                     "Select a Name:",
-                     sortedUniqueNames
-         )
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("nameTimeSeries"),
-         textOutput("test")
-      )
    )
 )
+   # Sidebar with a slider input for number of bins 
+   
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    output$test <- renderText(input$selected_gender)
    
+   # output$popular_plot <-renderText(filter(df, Year >= input$year_range[1] & Year <= input$year_range[2]))
+   output$popular_plot <-renderPlot({
+     filteredDF <- filter(df, Year >= input$year_range[1] & Year <= input$year_range[2]) %>% group_by(Name) %>% summarise(total = sum(n)) %>% arrange(desc(total),Name)
+     
+     ggplot(data=filteredDF[1:10,], aes(Name, total)) +
+       geom_col()
+   })
+
    
    output$nameTimeSeries <- renderPlot({
      filteredDF <- filter(df,Name == input$selected_name)
@@ -42,7 +80,7 @@ server <- function(input, output) {
      if (input$selected_gender != "E") {
        filteredDF <- filter(filteredDF,Gender == input$selected_gender)
      }
-     ggplot(data=filteredDF, aes(x=Year, y=Count, group = Gender, color=Gender)) +
+     ggplot(data=filteredDF, aes(x=Year, y=n, group = Gender, color=Gender)) +
        geom_line()+
        geom_point()
    })
--- data-cleaning.R
+++ data-cleaning.R
@@ -2,13 +2,13 @@ library(tidyverse)
 
 
 df <- read.csv("data/yob1880.txt", header = FALSE) %>%
-    rename(Name = V1, Gender = V2, Count =  V3) %>%
+    rename(Name = V1, Gender = V2, n =  V3) %>%
     mutate(Year = 1880)
 
 for(i in 1881:2016) {
   currentFile <- sprintf("data/yob%d.txt", i)
   df2 <- read.csv(currentFile, header = FALSE) %>%
-    rename(Name = V1, Gender = V2, Count =  V3) %>%
+    rename(Name = V1, Gender = V2, n =  V3) %>%
     mutate(Year = i)
   df <- rbind(df, df2)
   
